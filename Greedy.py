f684df769120d302c12485c8a66177de1de6704d
Arild Dalsgård <arild.dalsgaard@gmail.com>
2019-01-07 15:16:51 +0100

    Made overlapping dict.

--- Greedy.py
+++ Greedy.py
@@ -1,4 +1,6 @@
 from Solver import Solver
+import numpy as np
+
 
 # Greedy class inherits from solver
 
@@ -8,20 +10,39 @@ class Greedy(Solver):
         self.services = problem.services
         self.buses = problem.buses
         self.drivers = problem.drivers
-        self.candidateDrivers = []
-        self.candidateBuses = []
+        self.x = []
+        self.y = []
+        self.overtime = []
+        self.assignedBus = []
+        self.overlapping_dict = self.computeOverlapping()
 
-    def computeCandidates(self):
-        for service in self.services:
-            for bus in self.buses:
-                if self.validBusCandidate(service, bus):
-                    service.assignBus(bus)
-                    self.candidateBuses.append(service)
-            for driver in self.drivers:
-                if self.validDriverCandidate(service, driver):
-                    service.assignDriver(driver)
-                    self.candidateDrivers.append(service)
-        return [self.candidateBuses, self.candidateDrivers]
+
+
+
+    def isOverlapping(self, s1, s2):
+        s1_start = s1.getStartTime()
+        s1_dur = s1.getDurationMinutes()
+        s2_start = s2.getStartTime()
+        s2_dur = s2.getDurationMinutes()
+        start_overlapping = (s1_start + s1_dur < s2_start + s2_dur) and (s1_start + s1_dur > s2_start)
+        end_overlapping = (s1_start > s2_start) and (s1_start < s2_start + s2_dur)
+        return start_overlapping or end_overlapping
+
+    def computeOverlapping(self):
+        overlapping_dict = {}
+        for s1 in self.services:
+            overlapping_dict[s1] = []
+            for s2 in self.services:
+                if self.isOverlapping(s1, s2):
+                    overlapping_dict[s1].append(s2)
+        return overlapping_dict
+
+
+    def computeCandidate(self):
+        drivers = sorted(self.drivers, key=lambda x: x.getMaximumWorkingMinutes(), reverse=True)
+        services = sorted(self.services, key=lambda x: x.getDurationMinutes(), reverse=True)
+        for d in drivers:
+
 
 
     def validBusCandidate(self, service, bus):
